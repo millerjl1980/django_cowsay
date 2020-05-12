75603d81d25c171603fb67fba70fd1279a260514
Justin Miller <indymiller@gmail.com>
2020-05-12 10:46:56 -0400

    info from cowsay printing correctly to html using pre tag

--- cowsay/views.py
+++ cowsay/views.py
@@ -15,8 +15,6 @@ def index(request):
             # https://www.youtube.com/watch?v=2Fp1N6dof0Y
             output = subprocess.run(['cowsay', new_cow.text], capture_output=True, text=True)
             output = output.stdout
-            # https://chase-seibert.github.io/blog/2010/08/06/redirect-console-output-to-a-django-httpresponse.html
-            output = ['<BR>' if c == '\n' else c for c in output]
             return render(request, 'index.html', {'form': TextForm, 'output': output})
     else:
         form = TextForm()
